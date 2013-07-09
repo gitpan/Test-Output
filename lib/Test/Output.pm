9a47a18f8c8d762d89eb760cc6aed72fe3822d91
brian d foy <bdfoy@cpan.org>
2013-07-09 02:16:55 +0000

    Import of BDFOY/Test-Output-1.02 from CPAN.
    gitpan-cpan-distribution: Test-Output
    gitpan-cpan-version:      1.02
    gitpan-cpan-path:         BDFOY/Test-Output-1.02.tar.gz
    gitpan-cpan-author:       BDFOY
    gitpan-cpan-maturity:     released

--- lib/Test/Output.pm
+++ lib/Test/Output.pm
@@ -65,7 +65,7 @@ Version 0.16
 
 =cut
 
-$VERSION = '1.01_03';
+$VERSION = '1.02';
 
 =head1 SYNOPSIS
 
@@ -486,7 +486,7 @@ is functionally equivalent to
 except that $coderef is only executed once.
 
 Unlike, stdout_is() and stderr_is() which ignore STDERR and STDOUT
-repectively, output_is() requires both STDOUT and STDERR to match in order
+respectively, output_is() requires both STDOUT and STDERR to match in order
 to pass. Setting either $expected_stdout or $expected_stderr to C<undef>
 ignores STDOUT or STDERR respectively.
 
@@ -938,7 +938,7 @@ be notified of progress on your bug as I make changes.
 
 Thanks to chromatic whose TieOut.pm was the basis for capturing output.
 
-Also thanks to rjbs for his help cleaning the documention, and pushing me to
+Also thanks to rjbs for his help cleaning the documentation, and pushing me to
 L<Sub::Exporter>.
 
 Thanks to David Wheeler for providing code block support and tests.
@@ -947,7 +947,7 @@ Thanks to Michael G Schwern for the solution to combining STDOUT and STDERR.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2005-2008 Shawn Sorichetti, All Rights Reserved.
+Copyright 2005-2013 Shawn Sorichetti, All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
